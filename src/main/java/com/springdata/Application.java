da3e0a0ab09072f842ce0cd2bb4b8ee3fdd9cf2b
Slava <slavacheban22@gmail.com>
2019-05-13 07:56:08 +0300

    add main

--- src/main/java/com/springdata/Application.java
+++ src/main/java/com/springdata/Application.java
@@ -1,7 +1,10 @@
 package com.springdata;
 
+import com.springdata.config.ApplicationConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
     }
 }
